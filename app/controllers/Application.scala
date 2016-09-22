63b5bfd69761836003594ba8c8b7b7182f4b37c0
edina.kim <edina.kim@daumkakao.com>
2016-09-22 14:52:59 +0900

    twitter request

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,8 +1,18 @@
 package controllers
 
-import com.ning.http.client.oauth.{ConsumerKey, RequestToken}
+
 import core.Env
+import play.api.libs.oauth.{ConsumerKey, OAuthCalculator, RequestToken}
+import play.api.libs.ws.{WS, WSRequest}
 import play.api.mvc.{Action, Controller}
+
+import scala.concurrent.Future
+import play.api.Play.current
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import core.EnvParserInstance._
+import play.api.Logger
+import play.api.libs.iteratee.Iteratee    // 컴패니언 오브젝트로 이름이 EnvParser 이면 따로 import dksgownjeh ehla
 
 
 /**
@@ -11,19 +21,37 @@ import play.api.mvc.{Action, Controller}
 object Application extends Controller {
   import core.Env._
 
-  def index = Action { implicit request =>
-    // Response
-    Ok("hello world")
+  def index = Action.async { implicit request =>
+
+    val loggingIterates = Iteratee.foreach[Array[Byte]] { (array: Array[Byte]) =>
+      Logger.info(array.map(_.toChar).mkString)
+
+    }
+    credential().map{
+      case (ck: ConsumerKey, rt: RequestToken) =>
+      WS.url("https://stream.twitter.com/1.1/statuses/filter.json")
+        .sign(OAuthCalculator(ck, rt))
+        .withQueryString("track" -> "love")
+        .get { response =>
+          Logger.info("response status: " + response.status)
+          loggingIterates
+        }
+        .map{ _ => Ok("Stream closed")}
+    }.getOrElse {
+      Future.successful {
+        InternalServerError("Twitter credential")
+      }
+    }
   }
+//
+//  case class ConsmerKey(apiKey: String, apiSecret: String)
+//  case class RequestToken(token: String, tokenSecret: String)
 
-  case class ConsmerKey(apiKey: String, apiSecret: String)
-  case class RequestToken(token: String, tokenSecret: String)
-
-  def credential: Option[(ConsmerKey, RequestToken)] = {
-    val apiKey: Option[String] = Env.as[String]("twitter apiKey")
-    val apiSecret: Option[String] = as[String]("twitter apiSecret")
-    val token: Option[String] = as[String]("twitter token")
-    val tokenSecret: Option[String] = as[String]("twitter tokenSecret")
+  def credential(): Option[(ConsumerKey, RequestToken)] = {
+    val apiKey: Option[String] = Env.as[String]("twitter.apiKey")
+    val apiSecret: Option[String] = as[String]("twitter.apiSecret")
+    val token: Option[String] = as[String]("twitter.token")
+    val tokenSecret: Option[String] = as[String]("twitter.tokenSecret")
 
 
     // 1개라도 값이 없을 경우 None 반환
@@ -31,11 +59,11 @@ object Application extends Controller {
 
     // 1. option에서 값을 꺼내서 option(tuple)로 변환
     // 1-1. flatMap (Option[A] => f:A => Option[B])
-    val res: Option[(ConsmerKey, RequestToken)] = apiKey.flatMap {
+    val res: Option[(ConsumerKey, RequestToken)] = apiKey.flatMap {
       ak => apiSecret.flatMap {
         as => token.flatMap {
           t => tokenSecret.flatMap {
-            ts => Some(ConsmerKey(ak, as), RequestToken(t, ts))
+            ts => Some(ConsumerKey(ak, as), RequestToken(t, ts))
           }
         }
       }
@@ -43,17 +71,17 @@ object Application extends Controller {
     res
 
     // 1-2. for
-    val resFor: Option[(ConsmerKey, RequestToken)] = for {
+    val resFor: Option[(ConsumerKey, RequestToken)] = for {
       ak <- apiKey
       as <- apiSecret
       t <- token
       ts <- tokenSecret
-    } yield (ConsmerKey(ak, as), RequestToken(t, ts))
+    } yield (ConsumerKey(ak, as), RequestToken(t, ts))
     resFor
 
     // 1-3. match
-    val resMatch: Option[(ConsmerKey, RequestToken)] = (apiKey, apiSecret, token, tokenSecret) match {
-      case (Some(ak), Some(as), Some(t), Some(ts)) => Some(ConsmerKey(ak, as), RequestToken(t, ts))
+    val resMatch: Option[(ConsumerKey, RequestToken)] = (apiKey, apiSecret, token, tokenSecret) match {
+      case (Some(ak), Some(as), Some(t), Some(ts)) => Some(ConsumerKey(ak, as), RequestToken(t, ts))
       case _ => None
     }
     resMatch
