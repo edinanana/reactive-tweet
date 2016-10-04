9115df9ee39e520161750ad1ddcaba33e7b2b8c9
edina.kim <edina.kim@daumkakao.com>
2016-10-04 17:06:34 +0900

    iteratee, enumerator

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,7 +12,8 @@ import play.api.Play.current
 import scala.concurrent.ExecutionContext.Implicits.global
 import core.EnvParserInstance._
 import play.api.Logger
-import play.api.libs.iteratee.Iteratee    // 컴패니언 오브젝트로 이름이 EnvParser 이면 따로 import dksgownjeh ehla
+import play.api.libs.iteratee.{Concurrent, Iteratee}
+import play.api.libs.json.JsObject    // 컴패니언 오브젝트로 이름이 EnvParser 이면 따로 import dksgownjeh ehla
 
 
 /**
@@ -23,18 +24,49 @@ object Application extends Controller {
 
   def index = Action.async { implicit request =>
 
-    val loggingIterates = Iteratee.foreach[Array[Byte]] { (array: Array[Byte]) =>
-      Logger.info(array.map(_.toChar).mkString)
+    val loggingIterates = Iteratee.foreach[JsObject] { js =>
+      Logger.info(js.toString())
 
     }
+
+    // enumerator - source : 들어오는 데이터 (근원지)
+
+    // enumeratee - flow : 데이터의 흐름
+
+    // iteratee - sink : 데이터가 끝나는 지점 (목적지, 흡수)
+
+    // source ===> flow ===> sink ---> source ---> sink
+
+    //////////////////////////////////
+    import play.api.libs.iteratee._
+    import play.api.libs.json._
+    import play.extras.iteratees._
+
+    val (iteratee, enumerator) = Concurrent.joined[Array[Byte]]
+
+    // 데이터가 들어왔을때 어떻게 하겠다
+    val jsonStream: Enumerator[JsObject] = enumerator &> Encoding.decode() &> Enumeratee.grouped(JsonIteratees.jsSimpleObject)
+
+    // 데이터를 어떻게 소비하겠다
+    jsonStream run loggingIterates
+
+    // action(행위) : 실행을 통해서 이루어진 결과를 얻을 수 있다
+    // definition(정의) : 실행전의 프로그램
+
+    // iteratee ===> enumerator ===> jsonStream ===> loggingIteratee
+
+    // stream 을 위해 iteratee, enumerator 을 사용
+    //////////////////////////////////
+
+
     credential().map{
       case (ck: ConsumerKey, rt: RequestToken) =>
       WS.url("https://stream.twitter.com/1.1/statuses/filter.json")
         .sign(OAuthCalculator(ck, rt))
-        .withQueryString("track" -> "love")
+        .withQueryString("track" -> "박보검")
         .get { response =>
           Logger.info("response status: " + response.status)
-          loggingIterates
+          iteratee
         }
         .map{ _ => Ok("Stream closed")}
     }.getOrElse {
